81078cec1dbd8058aedf1fd83acd34d4a15cc2f8
Diva Intan <divaintan107@gmail.com>
2024-05-27 14:29:58 +0700

    praktikum10 & tugas10

--- resources/views/admin/partials/_sidebar.blade.php
+++ resources/views/admin/partials/_sidebar.blade.php
@@ -11,9 +11,17 @@
       </li><!-- End Dashboard Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#">
+        <a class="nav-link collapsed" href="/admin/student">
           <i class="bi bi-user"></i>
           <span>Student</span>
+        </a>
+      </li>
+
+      
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="/admin/courses">
+          <i class="bi bi-user"></i>
+          <span>Courses</span>
         </a>
       </li>
       
